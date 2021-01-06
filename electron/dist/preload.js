5fcacb76a71f789ed3f8ff4fc50d1a52adf21f4e
wolfpat01 <haithem2001@gmail.com>
2021-01-06 10:41:03 +0100

    lets try again? maybe

--- electron/dist/preload.js
+++
@@ -1,6 +0,0 @@
-global.exports = {};
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-window.addEventListener("DOMContentLoaded", function () {
-});
-//# sourceMappingURL=preload.js.map
